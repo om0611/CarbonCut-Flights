67d1204bf3c7d2835accb59d07f135d9844872dc
deekay99 <dhairyathakkar25@gmail.com>
2024-04-01 10:17:01 -0400

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -37,7 +37,6 @@ def calculate_flight_scores(flights: dict[tuple[str, tuple], list[int]],
 
 def optimal_routes(graph: data_classes.Graph, home_airport: str, dest_airport: str,
                    weights: tuple[float, float, float] = (0.1, 0.1, 0.8)) -> list[tuple]:
-
     home_vertex = graph.get_vertex(home_airport)
     destination_vertex = graph.get_vertex(dest_airport)
 
@@ -52,7 +51,8 @@ def optimal_routes(graph: data_classes.Graph, home_airport: str, dest_airport: s
 
     sorted_flights = sorted(flight_scores.items(), key=lambda item: item[1])
 
-    all_flights = [(flight[0], flights[flight[0]][0], flights[flight[0]][1], round(flight[1], 5) ) for flight in sorted_flights]
+    all_flights = [(flight[0], flights[flight[0]][0], flights[flight[0]][1], round(flight[1], 5)) for flight in
+                   sorted_flights]
     if len(all_flights) > 5:
         return all_flights[:4]
     else:
@@ -72,3 +72,27 @@ def all_countries(flight_path_file: str) -> set:
             countries.add(row[3])
 
     return countries
+
+
+def carbon_statistics(offset: int) -> set[str]:
+    all_stats = set()
+    avg_c02_percentage_person = round((offset / 4000000) * 100, 2)
+    car_miles = round(offset / 362, 2)
+    plastic_bottles = round(offset / 83, 2)
+    light_bulb = round(offset / 42)
+    coffee_cups = round(offset / 50)
+    all_stats.add(
+        f"Over {coffee_cups}! Thats how many coffee cups you saved by flying with VerdeVoyage. Our planet thanks you! ")
+    all_stats.add(
+        f"{light_bulb}. That is how many hours of having a light bulb turned on you have saved by flying with "
+        f"VerdeVoyage.")
+    all_stats.add(
+        f"{plastic_bottles}. That is how many plastic bottles you saved by choosing the most greenflight "
+        f"to your destination! Thank you for flying with VerdeVoyage.")
+    all_stats.add(
+        f"Choosing this flight over the others, you have saved the equivalent of not driving for {car_miles} miles. "
+        f"Thank you for making a greener planet!")
+    all_stats.add(
+        f"By flying with VerdeVoyage, you have saved {avg_c02_percentage_person}% of an individuals "
+        f"annual varbon usage.")
+    return all_stats
