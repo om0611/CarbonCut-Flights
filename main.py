7a2c80c5e7b09121e8202863714b5d120a85fc63
om0611 <omrock198@gmail.com>
2024-04-03 20:44:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,17 @@
 """
-This Python file contains the code to run our program.
+Module Description
+==================
+This module contains the code to run our entire program.
+
+Copyright and Usage Information
+===============================
+This file is provided exclusively for the use and benefit of customers of VerdeVoyage. Any form of
+distribution, reproduction, or modification of this code outside of its intended use within the VerdeVoyage
+platform is strictly prohibited. All rights reserved.
+
+This file is Copyright (c) 2024 VerdeVoyage
 """
-import csv
-import data_classes
 import helper_functions
-import flight_visualization
 
-
-# Example Code
-home_airport = 'YYZ'
-graph = data_classes.Graph()
-
-with open('CSV Files/flight_data.csv', 'r') as file:
-    reader = csv.reader(file)
-    next(reader, None)          # skip the header
-    for row in reader:
-        if row[0] == home_airport:
-            graph.add_vertex(row[0], row[1], (0,0))
-            graph.add_vertex(row[2], row[3], (0,0))
-
-            # If any of the values are missing, then move to the next row
-            if row[4] == '' or row[12] == '' or row[14] == '':
-                continue
-
-            aircrafts = tuple(row[4].split('|'))
-            airline = row[6].split('| ')[0].strip('[]')
-            stops = int(row[11])
-            price = float(row[12])
-            emissions = int(row[14])
-
-            flight_package = (airline, aircrafts)
-            flight_info = [price, stops, emissions]
-            graph.add_edge(row[0], row[2], (flight_package, flight_info))
-
-airport_codes = {}
-with open('CSV Files/78_airport_info.csv', 'r') as file:
-    reader = csv.reader(file)
-    next(reader, None)      #skip the header
-    for row in reader:
-        airport_codes[row[0]] = (float(row[1]), float(row[2]))
-
-for vertex in data_classes.Graph.all_verticies(graph):
-    vertex.cordinates = airport_codes[vertex.airport_code]
+if __name__ == '__main__':
+    helper_functions.run_voyage()
